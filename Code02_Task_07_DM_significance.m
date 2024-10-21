0c1d8567be036d41d71661f055babc057f6489e8
YJ-0000 <ys1j13@gmail.com>
2024-10-21 16:27:47 +0900

    check DM significance during tasks

---
+++ Code02_Task_07_DM_significance.m
@@ -0,0 +1,64 @@
+clear; clc; close all;
+
+test_for = 'phase'; % magnitude or phase
+
+%% Load data
+% Load Resting State Data
+load('DMs/DM_cortical_subcortical_ext_fbDMD_noROInorm_indiv_10.mat');
+D_rest = D(2:end, tau ~= 0).^(0.72/1.5);
+sub_ids_rest = sub_ids(tau ~= 0);
+
+% List of task files and names
+task_names = {'WM','EMOTION','MOTOR','LANGUAGE','GAMBLING','SOCIAL','RELATIONAL'};
+num_tasks = length(task_names);
+D_tasks = cell(1, num_tasks);
+sub_ids_tasks = cell(1, num_tasks);
+
+% Load Task Data
+for i = 1:num_tasks
+    load(['DMs\DM_tfMRI_', task_names{i} ,'_cortical_subcortical_ext_fbDMD_noROInorm_indiv_10.mat']);
+    D_tasks{i} = D(2:end, tau ~= 0);
+    sub_ids_tasks{i} = sub_ids(tau ~= 0);
+end
+
+% Find Common Subjects Across All Datasets
+common_sub_ids = sub_ids_rest;
+for i = 1:num_tasks
+    common_sub_ids = intersect(common_sub_ids, sub_ids_tasks{i});
+end
+
+% Subset Rest Data to Common Subjects
+[~, idx_rest] = ismember(common_sub_ids, sub_ids_rest);
+D_rest = D_rest(:, idx_rest);
+sub_ids_rest = sub_ids_rest(idx_rest);
+
+% Subset Task Data to Common Subjects
+for i = 1:num_tasks
+    [~, idx_task] = ismember(common_sub_ids, sub_ids_tasks{i});
+    D_tasks{i} = D_tasks{i}(:, idx_task);
+    sub_ids_tasks{i} = sub_ids_tasks{i}(idx_task);
+end
+
+%% t-test
+idx_order = [1,5,9,7,3];
+
+phase_rest = angle(D_rest(idx_order,:));
+phase_tasks = cell(1, num_tasks);
+for i = 1:num_tasks
+    phase_tasks{i} = angle(D_tasks{i}(idx_order,:));
+end
+
+for n_dm = 1:5
+    [h,p] = ttest(phase_rest(n_dm,:));
+    fprintf('Significance of DM%d: mean theta = %.6f , p=%.6f \n',n_dm, mean(phase_rest(n_dm,:)), p);
+end
+disp(' ');
+
+for n_task = 1:num_tasks
+    phase_task_1 = phase_tasks{n_task};
+    for n_dm = 1:5
+        [h,p] = ttest(phase_task_1(n_dm,:));
+        fprintf('Significance of DM%d during %s: mean theta = %.6f , p=%.6f \n',n_dm,task_names{n_task}, mean(phase_task_1(n_dm,:)), p);
+    end
+    disp(' ');
+end
